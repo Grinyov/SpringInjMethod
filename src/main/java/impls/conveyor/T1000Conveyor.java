f18e96a3ba9c27c13c278c9c48fab714ba35d8a6
green <vgrinyov@gmail.com>
2015-08-26 15:18:20 +0300

    Initial commit

---
+++ src/main/java/impls/conveyor/T1000Conveyor.java
@@ -0,0 +1,11 @@
+package impls.conveyor;
+
+import interfaces.RobotConveyor;
+import interfaces.Robot;
+
+public abstract class T1000Conveyor implements RobotConveyor {
+
+	//@Override
+	public abstract Robot createRobot();
+
+}
